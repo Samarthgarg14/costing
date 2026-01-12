0de08e6ba05cc3b4c7e331bed5ef4eff50bbbe5b
StackxCoder <amporacables@gmail.com>
2026-01-12 15:56:27 +0530

    Prepare for Render deployment

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,16 @@
 import os
 
+# Server socket
+bind = '0.0.0.0:' + os.environ.get('PORT', '10000')
+
+# Worker processes
 workers = int(os.environ.get('GUNICORN_WORKERS', '2'))
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = '0.0.0.0:' + os.environ.get('PORT', '10000')
 
+# Logging
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+# Application
+wsgi_app = 'app:app'
